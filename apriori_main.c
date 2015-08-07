aa4a2d8034ebf6ff82315d5ce74abbedffaba85a
Nyah Check <check.nyah@gmail.com>
2015-08-07 10:16:13 +0100

    Initialized candidate 2 itemset array for saving the next candidate itemsets.

--- apriori_main.c
+++ apriori_main.c
@@ -21,8 +21,13 @@ int main(int argc, char** argv) {
         if (i == 1) {
             candidate_frequencies1 = calloc(ONE_ITEMSET_ARRAY_MAX, sizeof(int));   //1-itemset frequencies
             frequent_frequencies1 = calloc(ONE_ITEMSET_ARRAY_MAX, sizeof(int ));   //1-itemset frequencies
+            candidate_frequencies_cur = calloc(OTH_ITEMSET_ARRAY_MAX, sizeof(struct itemsetPtr));// 2- candidate itemsets
+            for (j=0;j <OTH_ITEMSET_ARRAY_MAX;j++) {
+                candidate_frequencies_cur[j].itemset_ptr = NULL;
+            }
+            
         } else if (i == 2) {
-            candidate_frequencies_cur = calloc(OTH_ITEMSET_ARRAY_MAX, sizeof(struct itemsetPtr));
+        	candidate_frequencies_cur = calloc(OTH_ITEMSET_ARRAY_MAX, sizeof(struct itemsetPtr));
             frequent_frequencies_cur = calloc(OTH_ITEMSET_ARRAY_MAX,  sizeof(struct itemsetPtr));
             for (j=0;j <OTH_ITEMSET_ARRAY_MAX;j++) {
                 candidate_frequencies_cur[j].itemset_ptr = NULL;
