aac47ce1d22cb9d9cdef20c5c50bf4e73602d602
Nyah Check <check.nyah@gmail.com>
2015-08-24 09:30:55 +0100

    Adding preprocessing directives to header, Fixing header declaration for fptree

---
+++ fptree.h
@@ -0,0 +1,34 @@
+/*
+ * File: apriori.h
+ * Purpose: Defines the functions and structures to be used in the FP Growth algorithm.
+ * Author: Bawe Emmanuel
+ *
+ */
+ 
+#ifndef FPTREE_H
+#define FPTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define TRANS_DB "retail.dat.tmp" /* Transactional database */
+
+#define BASKET_MAX_CHARS 100000
+#define BASKET_MAX_ITEMS 1000
+#define ITEM_MAX 10
+#define ONE_ITEMSET_ARRAY_MAX 16500
+#define OTH_ITEMSET_ARRAY_MAX 524287
+#define ITEMSET_MAX_SIZE 10
+#define SUPPORT_THRESHOLD 5
+
+
+
+
+
+
+
+
+
+#endif
