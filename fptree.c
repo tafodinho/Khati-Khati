f4eced10079af89666d1869794dd5443c4fb0758
Nyah Check <check.nyah@gmail.com>
2015-08-24 10:36:50 +0100

    Added functions to create fptree nodes and fpgrowth sub tree nodes

---
+++ fptree.c
@@ -0,0 +1,25 @@
+/**
+ * Filename: apriori.c
+ * Purpose: Defines the functions for the FP Growth algorithm as used
+ * Author: Bawe Emmanuel
+ */
+#include "fptree.h"
+
+
+fptreePtr create_fptree_node(int sup) {
+	fptreePtr new_node = (struct fptreePtr)malloc ((sizeof(struct fptreePtr));
+	new_node->support = sup;
+	new_node->num_nodes++;
+	
+	return new_node;
+}
+
+fpgsubtreePtr create_fpsubtree_node(int name, int support, fpgsubtreePtr prev) {
+	fpgsubtreePtr new_node = (struct fpgsubtreePtr)malloc(sizeof(struct fpgsubtreePtr));
+	new_node->item_name = name;
+	new_node->item_count = support;
+	new_name->parent = prev;
+	
+	return new_node;
+}
+	
