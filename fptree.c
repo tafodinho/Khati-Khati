79bde2e047ea14d2001aa1dd342aa427f8324286
Nyah Check <check.nyah@gmail.com>
2015-08-28 20:56:32 +0100

    Removes ancestor_pruning since in frequent sets are not added to tree. Saves alot of computing memory

--- fptree.c
+++ fptree.c
@@ -176,7 +176,7 @@ void start_mining2(fptreePtr fptree, fpgsubtreePtr node, int item, int itemset_s
 		count_fpgsingles(fptree); /* Count singles in linked list */
 		lheader = create_local_htable(count); /* Create and pop local header table */
 		if (lheader != NULL) {
-			prune_ancestors(fptree, count); /* Prune ancestor itemsets */
+			//prune_ancestors(fptree, count); /* Prune ancestor itemsets */
 			ltree->root = gen_local_fptree(fptree, lheader); /* Create new local root for local FP tree */
 			start_mining2(ltree, lheader->node,lheader->item_name, code_sofar, size + 1); /* Mine new FP tree */
 		}
